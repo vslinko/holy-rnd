346093033032b9a47d2a773cc5eaecfc2807c147
Viacheslav Slinko <vslinko@yahoo.com>
2019-11-09 14:39:25 +0300

    WIP

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-function rand() {
-  return Math.round(Math.random());
+function getDistance(p1, p2) {
+  return Math.round(Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2)))
 }
 
-let { x, y } = API.getCurrentPosition();
+const me = API.getCurrentPosition();
+const size = API.getArenaSize();
 
-if (rand()) {
-  x = rand() ? x + 1 : x - 1;
-} else {
-  y = rand() ? y + 1 : y - 1;
+const enemies = API.getEnemies().map(e => {
+  return {
+    ...e.position,
+    distance: getDistance(me, e.position),
+  }
+});
+
+const nearestEnemy = enemies.concat().sort((a, b) => a.distance - b.distance)[0]
+
+const diffX = me.x != nearestEnemy.x
+const diffY = me.y != nearestEnemy.y
+const moveType = diffX && diffY ? 'DIAGONAL' : (
+  diffX ? 'X' : 'Y'
+)
+
+switch (moveType) {
+  case 'DIAGONAL':
+    API.move(
+      me.x + (nearestEnemy.x > me.x ? 1 : -1),
+      me.y + (nearestEnemy.y > me.y ? 1 : -1),
+    );
+    break;
+  case 'X':
+      API.move(
+        me.x + Math.max(-2, Math.min(2, nearestEnemy.x - me.x)),
+        me.y
+      );
+      break;
+  case 'Y':
+      API.move(
+        me.x,
+        me.y + Math.max(-2, Math.min(2, nearestEnemy.y - me.y)),
+      );
+      break;
 }
-
-API.move(x, y);
