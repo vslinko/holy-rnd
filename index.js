101a5e3fdd8a6bc776c9e8683a8bc04a5af46fcd
Viacheslav Slinko <vslinko@yahoo.com>
2019-11-09 16:15:24 +0300

    WIP

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// function getDistance(p1, p2) {
+//   return Math.round(Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2)))
+// }
 function getDistance(p1, p2) {
-  return Math.round(Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2)))
+  return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y)
 }
 
 const me = API.getCurrentPosition();
-const size = API.getArenaSize();
 
 const enemies = API.getEnemies().map(e => {
   return {
@@ -21,54 +23,58 @@ const moveType = diffX && diffY ? 'DIAGONAL' : (
   diffX ? 'X' : 'Y'
 )
 
-switch (moveType) {
-  case 'DIAGONAL':
-    if (nearestEnemy.distance == 4) {
-      API.move(
-        me.x,
-        me.y
-      );
-    } else {
-      API.move(
-        me.x + (nearestEnemy.x > me.x ? 1 : -1),
-        me.y + (nearestEnemy.y > me.y ? 1 : -1),
-      );
-    }
-    break;
-  case 'X':
-      if (nearestEnemy.distance == 4) {
-        API.move(
-          me.x + Math.max(-1, Math.min(1, nearestEnemy.x - me.x)),
-          me.y
-        );
-      } else if (nearestEnemy.distance == 3) {
-        API.move(
-          me.x,
-          me.y
-        );
-      } else {
-        API.move(
-          me.x + Math.max(-2, Math.min(2, nearestEnemy.x - me.x)),
-          me.y
-        );
-      }
-      break;
-  case 'Y':
-      if (nearestEnemy.distance == 4) {
-        API.move(
-          me.x,
-          me.y + Math.max(-1, Math.min(1, nearestEnemy.y - me.y)),
-        );
-      } else if (nearestEnemy.distance == 3) {
-        API.move(
-          me.x,
-          me.y,
-        );
-      } else {
-        API.move(
-          me.x,
-          me.y + Math.max(-2, Math.min(2, nearestEnemy.y - me.y)),
-        );
-      }
-      break;
+if (nearestEnemy.distance === 4) {
+  // continue
+} else if (nearestEnemy.distance < 4) {
+  API.move(nearestEnemy.x, nearestEnemy.y)
+} else {
+  API.move(
+    me.x + (nearestEnemy.x == 0 ? 0 : (nearestEnemy.x > me.x ? 1 : -1)),
+    me.y + (nearestEnemy.y == 0 ? 0 : (nearestEnemy.y > me.y ? 1 : -1)),
+  );
 }
+
+// switch (moveType) {
+//   case 'DIAGONAL':
+//     API.move(
+//       me.x + (nearestEnemy.x > me.x ? 1 : -1),
+//       me.y + (nearestEnemy.y > me.y ? 1 : -1),
+//     );
+//     break;
+//   case 'X':
+//       if (nearestEnemy.distance == 5) {
+//         API.move(
+//           me.x + Math.max(-1, Math.min(1, nearestEnemy.x - me.x)),
+//           me.y
+//         );
+//       } else if (nearestEnemy.distance == 4) {
+//         API.move(
+//           me.x,
+//           me.y
+//         );
+//       } else {
+//         API.move(
+//           me.x + Math.max(-3, Math.min(3, nearestEnemy.x - me.x)),
+//           me.y
+//         );
+//       }
+//       break;
+//   case 'Y':
+//       if (nearestEnemy.distance == 5) {
+//         API.move(
+//           me.x,
+//           me.y + Math.max(-1, Math.min(1, nearestEnemy.y - me.y)),
+//         );
+//       } else if (nearestEnemy.distance == 4) {
+//         API.move(
+//           me.x,
+//           me.y,
+//         );
+//       } else {
+//         API.move(
+//           me.x,
+//           me.y + Math.max(-3, Math.min(3, nearestEnemy.y - me.y)),
+//         );
+//       }
+//       break;
+// }
