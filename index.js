210d9a4c19f2cf1a2c1f629c5d162b3107c4df8a
Viacheslav Slinko <vslinko@yahoo.com>
2019-11-09 18:50:33 +0300

    WIP

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ function getDistance(p1, p2) {
 }
 
 const me = API.getCurrentPosition();
-const enemies = API.getEnemies().map(e => e.position);
+const enemies = API.getEnemies();
 
 const map = new Array(API.getArenaSize()).fill(0)
   .map(() => new Array(API.getArenaSize()).fill(0));
 
 enemies.forEach((e) => {
-  for (let x = Math.max(0, e.x - 4); x < Math.min(e.x + 4, API.getArenaSize()); x++) {
-    for (let y = Math.max(0, e.y - 4); y < Math.min(e.y + 4, API.getArenaSize()); y++) {
-      if (x == e.x && y == e.y) {
-        if (getDistance(me, e) <= API.getActionPointsCount()) {
+  for (let x = Math.max(0, e.position.x - 4); x < Math.min(e.position.x + 4, API.getArenaSize()); x++) {
+    for (let y = Math.max(0, e.position.y - 4); y < Math.min(e.position.y + 4, API.getArenaSize()); y++) {
+      if (x == e.position.x && y == e.position.y) {
+        if (getDistance(me, e.position) <= API.getActionPointsCount()) {
           map[x][y] += 2
-        } else {
+        } else if (e.name !== 'nin-jin') {
           map[x][y] -= 10
         }
-      } else if (getDistance({x,y}, e) <= 3) {
+      } else if (getDistance({x,y}, e.position) <= 3) {
         map[x][y] -= 10
       }
     }
@@ -34,7 +34,7 @@ for (let x = Math.max(0, me.x - 4); x < Math.min(me.x + 4, API.getArenaSize()); 
     const couldMove = myCell || distanceFromMe <= API.getActionPointsCount()
     if (couldMove) {
       if (map[x][y] == 0 && !myCell && distanceFromMe <= 2) {
-        const minDistance = Math.min(...enemies.map(e => getDistance({ x, y }, e)))
+        const minDistance = Math.min(...enemies.map(e => getDistance({ x, y }, e.position)))
         if (minDistance > 0) {
           map[x][y] += 1/minDistance
         }
