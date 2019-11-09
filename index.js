1c95bcc5dfdf30734872ebb99203ac7d6731a84a
Viacheslav Slinko <vslinko@yahoo.com>
2019-11-09 16:44:13 +0300

    WIP

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function getDistance(p1, p2) {
+  return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2))
+}
+function getMoves(p1, p2) {
   return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y)
 }
 
@@ -8,40 +11,46 @@ const enemies = API.getEnemies().map(e => {
   return {
     ...e.position,
     distance: getDistance(me, e.position),
+    moves: getMoves(me, e.position),
   }
 });
 
 const nearestEnemy = enemies.concat().sort((a, b) => a.distance - b.distance)[0]
 
-const diffX = me.x != nearestEnemy.x
-const diffY = me.y != nearestEnemy.y
-const oddDistance = nearestEnemy.distance % 2 == 0
-const moveType = diffX && diffY ? 'DIAGONAL' : (
-  diffX ? 'X' : 'Y'
-)
 
-console.log(me, nearestEnemy)
+console.log(me, nearestEnemy, API.getActionPointsCount())
 
-if (nearestEnemy.distance === 5) {
-  if (diffX) {
-    console.log('5x')
+if (nearestEnemy.distance <= API.getActionPointsCount()) {
+  console.log('<=3')
+  API.move(nearestEnemy.x, nearestEnemy.y)
+} else if (nearestEnemy.distance > 3 && nearestEnemy.distance <= 4) {
+  if (me.x != nearestEnemy.x) {
+    console.log('~4x')
+    API.move(
+      me.x + (nearestEnemy.x == 0 ? 0 : (nearestEnemy.x > me.x ? -1 : 1)),
+      me.y,
+    );
+    } else {
+    console.log('~4y')
+    API.move(
+      me.x,
+      me.y + (nearestEnemy.y == 0 ? 0 : (nearestEnemy.y > me.y ? -1 : 1)),
+    );
+  }
+} else if (nearestEnemy.distance > 4 && nearestEnemy.distance <= 5) {
+  if (me.x != nearestEnemy.x) {
+    console.log('~5x')
     API.move(
       me.x + (nearestEnemy.x == 0 ? 0 : (nearestEnemy.x > me.x ? 1 : -1)),
       me.y,
-      );
+    );
     } else {
-    console.log('5y')
+    console.log('~5y')
     API.move(
       me.x,
       me.y + (nearestEnemy.y == 0 ? 0 : (nearestEnemy.y > me.y ? 1 : -1)),
     );
   }
-} else if (nearestEnemy.distance === 4) {
-  console.log('4')
-  // continue
-} else if (nearestEnemy.distance < 4) {
-  console.log('<4')
-  API.move(nearestEnemy.x, nearestEnemy.y)
 } else {
   console.log('>5')
   API.move(
@@ -51,48 +60,3 @@ if (nearestEnemy.distance === 5) {
 }
 
 console.log()
-
-// switch (moveType) {
-//   case 'DIAGONAL':
-//     API.move(
-//       me.x + (nearestEnemy.x > me.x ? 1 : -1),
-//       me.y + (nearestEnemy.y > me.y ? 1 : -1),
-//     );
-//     break;
-//   case 'X':
-//       if (nearestEnemy.distance == 5) {
-//         API.move(
-//           me.x + Math.max(-1, Math.min(1, nearestEnemy.x - me.x)),
-//           me.y
-//         );
-//       } else if (nearestEnemy.distance == 4) {
-//         API.move(
-//           me.x,
-//           me.y
-//         );
-//       } else {
-//         API.move(
-//           me.x + Math.max(-3, Math.min(3, nearestEnemy.x - me.x)),
-//           me.y
-//         );
-//       }
-//       break;
-//   case 'Y':
-//       if (nearestEnemy.distance == 5) {
-//         API.move(
-//           me.x,
-//           me.y + Math.max(-1, Math.min(1, nearestEnemy.y - me.y)),
-//         );
-//       } else if (nearestEnemy.distance == 4) {
-//         API.move(
-//           me.x,
-//           me.y,
-//         );
-//       } else {
-//         API.move(
-//           me.x,
-//           me.y + Math.max(-3, Math.min(3, nearestEnemy.y - me.y)),
-//         );
-//       }
-//       break;
-// }
